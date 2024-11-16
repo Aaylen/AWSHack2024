fbf85218b82f9ef3e30fa18a109b8934d868200f
Aaylen <tubbyaylen@gmail.com>
2024-11-16 11:42:23 -0800

    added calculate average

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,12 @@
 from flask import Flask
-from backend.routes.stocks import server1
+from backend.routes.stocks import stocks
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
 # Register blueprints
-app.register_blueprint(server1, url_prefix='/stocks')
-
+app.register_blueprint(stocks, url_prefix='/stocks')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- backend/routes/stocks.py
+++ backend/routes/stocks.py
@@ -1,8 +1,40 @@
 from flask import Blueprint, jsonify, request
+import yfinance as yf
 
 stocks = Blueprint('stocks', __name__)
 
 @stocks.route('/endpoint', methods=['POST'])
 def post_endpoint():
+    data = request.get_json()
+    if 'action' in data:
+        if data['action'] == 'calculateAverage':
+            calculateAverage(data)
+        if data['action'] == 'bestDays':
+            pass
     
-    return jsonify()
+    return jsonify(data) 
+
+def calculateAverage(data):
+    tickerSymbol = data['stock']
+    stock = yf.Ticker(tickerSymbol)
+    startDate = data['startDate']
+    endDate = data['endDate']
+    historical_data = stock.history(start=startDate, end=endDate)
+    average = historical_data['Close'].mean()
+    return average
+
+def bestDays(data):
+    startDate = data['startDate']
+    endDate = data['endDate']
+    tickerSymbol = data['stock']
+    days = data['days']
+    stock = yf.Ticker(tickerSymbol)
+    historical_data = stock.history(start=startDate, end=endDate)
+    sorted_data = historical_data.sort_values(by='Close', ascending=False)
+    top_days = sorted_data.head(days).index.strftime('%Y-%m-%d').tolist()
+    return top_days
+    stock = yf.Ticker(tickerSymbol)
+    historical_data = stock.history(start=startDate, end=endDate)
+
+
+
